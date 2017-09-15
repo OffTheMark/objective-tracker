a850a10f0fd95a8b465290a493c39e2dce115ca0
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-09-15 17:48:46 -0400

    Create models and their migration
    App tracker app to installed apps

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Objective(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    target = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    date_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class TimeEntry(models.Model):
+    objective = models.ForeignKey(Objective, on_delete=models.CASCADE)
+    explanation = models.TextField()
+    effort = models.DecimalField(max_digits=6, decimal_places=2)
+    date_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.explanation
