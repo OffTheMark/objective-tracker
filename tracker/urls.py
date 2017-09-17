8997b8c48ad97d0dfcc9ee344237794265ead354
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-09-16 20:42:14 -0400

    Add sign up form with basic manual error handling

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signin$', views.SigninView.as_view(), name='signin'),
     url(r'^signout$', views.signout, name='signout'),
+    url(r'^signup$', views.SignupView.as_view(), name='signup'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import TemplateView
@@ -34,3 +35,34 @@ class SigninView(TemplateView):
 def signout(request):
     logout(request)
     return HttpResponseRedirect(reverse("tracker:index"))
+
+
+class SignupView(TemplateView):
+    template_name = "tracker/"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "tracker/signup.html")
+
+    def post(self, request):
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        error = None
+
+        if password != confirm_password:
+            error = "Passwords don't match."
+        elif User.objects.filter(username=username).exists():
+            error = "A user with this username already exists."
+        elif User.objects.filter(email=email).exists():
+            error = "A user with this email already exists."
+
+        if error is not None:
+            context = {"error": error}
+            return render(request, "tracker/signup.html", context)
+        else:
+            user = User.objects.create_user(username, email=email, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse("tracker:index"))
+
