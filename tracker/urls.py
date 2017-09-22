98f81ea9d53c536b55a38ad7e99562faea5fd9c7
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-09-22 15:50:58 -0400

    Fix small issue with URLs and overview reloading

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^signout$', views.signout, name='signout'),
     url(r'^signup$', views.SignupView.as_view(), name='signup'),
     url(r'^signin', views.SigninView.as_view(), name='signin'),
-    url(r'^dashboard', views.DashboardOverviewView.as_view(), name="dashboard/overview"),
+    url(r'^dashboard/$', views.DashboardOverviewView.as_view(), name="dashboard/overview"),
     url(r'^dashboard/objectives$', views.objectives, name="dashboard/objectives")
 ]
