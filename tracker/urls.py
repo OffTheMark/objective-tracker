7a595cce84715447e32d7fa09af4a5deddcb3c5a
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-09-26 16:04:37 -0400

    Correct small details in URLs

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'tracker'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signout$', views.signout, name='signout'),
-    url(r'^signup$', views.SignupView.as_view(), name='signup'),
+    url(r'^signout/$', views.signout, name='signout'),
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^signin', views.SigninView.as_view(), name='signin'),
     url(r'^entry/$', views.TimeEntryView.as_view(), name="entry"),
     url(r'^dashboard/$', views.DashboardOverviewView.as_view(), name="dashboard/overview"),
-    url(r'^dashboard/objectives$', views.objectives, name="dashboard/objectives")
+    url(r'^dashboard/objectives$', views.objectives, name="dashboard/objectives"),
 ]
